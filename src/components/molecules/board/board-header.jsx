e872ebbc7b419d141fc31b9b9c8720bd77789def
kimlong <kimlong5244@gmail.com>
2025-06-23 17:13:43 +0700

    refactor: adjust ui in BoardHeader component

--- src/components/molecules/board/board-header.jsx
+++ src/components/molecules/board/board-header.jsx
@@ -3,8 +3,8 @@ import { LayoutGrid } from "lucide-react";
 
 export const BoardHeader = () => {
   return (
-    <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4 mb-6 border-b pb-4">
-      <div className="flex items-center gap-3">
+    <div className="flex justify-between items-center gap-4 mb-6 border-b pb-4">
+      <div className="flex items-center gap-3 ">
         <div className="p-2 bg-primary/10 rounded-lg">
           <LayoutGrid className="h-6 w-6 text-primary" />
         </div>
